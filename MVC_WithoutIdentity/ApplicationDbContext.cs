0285b838542f0b1a9c1c7e18cd9a5572c831961f
Sviatoslav Samoilenko <Sviatoslav_Samoilenko@epam.com>
2022-08-06 13:04:53 +0300

    Add project files.

---
+++ MVC_WithoutIdentity/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+using MVC_WithoutIdentity;
+
+namespace MVC_WithoutIdentity
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext([NotNullAttribute] DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<MVC_WithoutIdentity.Good> Good { get; set; }
+    }
+}
---
+++ MVC_WithoutIdentity/Good.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_WithoutIdentity
+{
+    public class Good
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public double Price { get; set; }
+        public int MinBuy { get; set; }
+    }
+}
